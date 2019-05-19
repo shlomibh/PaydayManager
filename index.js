4bb1e4c51f7fe5a06c963f2009866e81afc02ebc
SHLOMI BEN HAMO <shlomibenhamo@SHLOMIs-MacBook-Pro.local>
2019-05-19 21:58:04 +0300

    first init

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const bodyParser= require('body-parser');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+.then(() => {
+    console.log('MongoDB is connected');
+})
+.catch((err) => {
+    console.log('erro occured in connection to mongo');
+    console.log(err);
+});
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const PORT = process.env.PORT | 5000;
+
+app.get('/', (req, res) => {
+    
+    res.send('Hello Shlomi');
+} );
+
+app.listen(PORT, () => {
+    console.log(`Server listennig on http://localhost:${PORT}`)
+});
