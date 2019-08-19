857f912e81fd8f8a28e12b67a04723dfc86efdf8
SHLOMI BEN HAMO <shlomibenhamo@SHLOMIs-MacBook-Pro.local>
2019-08-19 21:55:46 +0300

    login full

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const keys = require("./config/keys");
 require("./models/Users");
 require("./config/passport");
-const Users = require('./models/Users');
+// const Users = require('./models/Users');
 
 mongoose
   .connect(keys.mongoURI, { useNewUrlParser: true })
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listennig on http://localhost:${PORT}`);
 
-  const user = new Users({username:'shlomi',firstName:'shlomib',lastName:'ben hamo',phoneNumber:'0525011672',department:'software',role:'admin',email:'shlomi@walla.com',id:'305504565'});
-  user.setPassword('1234');
-  user.save();
+  // const user = new Users({username:'shlomi',firstName:'shlomib',lastName:'ben hamo',phoneNumber:'0525011672',department:'software',role:'admin',email:'shlomi@walla.com',id:'305504565'});
+  // user.setPassword('1234');
+  // user.save();
 });
