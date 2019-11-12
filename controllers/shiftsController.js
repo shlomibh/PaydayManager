e24f6617a9cc986c5a349828ba503eacdfbb9226
SHLOMI BEN HAMO <shlomibenhamo@SHLOMIs-MacBook-Pro.local>
2019-11-12 16:58:21 +0200

    hour fixed

--- controllers/shiftsController.js
+++ controllers/shiftsController.js
@@ -6,18 +6,15 @@ async function postShift(req, res, next) {
     try {
         const shift = req.body.shift;
         const id = shift.employeeId;
+        let ans;
         const user = await User.findOne({
             _id: id
         });
         if (!user) return res.status(httpCodes.CONFLICT).send("there is no such user");
         const department = user.department;
         shift.department = department;
-       // console.log(shift);
         existedShift = await Shift.find({ employeeId: shift.employeeId, date: shift.date });
-        console.log('existedShift');
-        console.log(existedShift);
-        console.log('after');
-        if(existedShift){
+        if(existedShift.length > 0){
             if(shift.absent === "חופש" || shift.absent === "מחלה") { //if we want to add absent shift
                 existedShift.forEach(element => {
                     if(element) return res.status(httpCodes.FORBIDDEN).send("there is shifts in this date");
@@ -29,18 +26,24 @@ async function postShift(req, res, next) {
                         return res.status(httpCodes.FORBIDDEN).send("the user absent in this day");
                     else{
                         if(element._id !== shift._id){
-                            checkHourValidation(res, element, shift);
+                            ans = checkHourValidation(res, element, shift);
+                            if(!ans){
+                                return res.status(httpCodes.CONFLICT).send("hours error");
+                            } 
                         }
                        }
-    
                 });
             }
-           
         }
-        const shiftFromDb = await Shift.create(shift);
-        if (!shiftFromDb) return res.status(httpCodes.FORBIDDEN).send("cannot create this shift");
-        //console.log(shiftFromDb);
-        return res.status(httpCodes.OK).send(shiftFromDb);
+        console.log('existedShift');
+        console.log(existedShift);
+        if(ans || existedShift.length < 1){
+            const shiftFromDb = await Shift.create(shift);
+            console.log(shiftFromDb);
+            if (!shiftFromDb) return res.status(httpCodes.FORBIDDEN).send("cannot create this shift");
+            return res.status(httpCodes.OK).send(shiftFromDb);
+        } 
+        
     } catch (error) {
         next(error);
     }
@@ -53,7 +56,6 @@ async function getShifts(req, res, next) {
             employeeId: employeeId
         });
         if (!shifts) return res.status(httpCodes.FORBIDDEN).send("there is no shifts to that employee");
-        console.log(shifts);
         return res.status(httpCodes.OK).send(shifts);
     } catch (error) {
         next(error);
@@ -67,7 +69,6 @@ async function deleteShift(req, res, next) {
             _id: shiftId
         });
         if (!shift) return res.status(httpCodes.FORBIDDEN).send("There is no such shift");
-        console.log(shift);
         await Shift.deleteOne({
             _id: shift._id
         });
@@ -79,42 +80,29 @@ async function deleteShift(req, res, next) {
 
 function checkHourValidation(res, element, shift) {
     const startFromDb = element.start;
-    const endFromDb = element.end; 
-    console.log('checkHourValidation');
-    console.log(element);
-    console.log(shift);
-    console.log('-------');  
+    const endFromDb = element.end;
     if(!checkHours(startFromDb, shift.start)) {
-        return res.status(httpCodes.CONFLICT).send("start hour confilct");
+        return false;
     }
     else if(!checkHours(endFromDb, shift.end)){
-        return res.status(httpCodes.CONFLICT).send("end hour confilct");
+        return false;
     }
     else if(!checkHours(endFromDb, shift.start)){
-        return res.status(httpCodes.CONFLICT).send("end-start hour confilct");
+        return false;
     }
+    return true;
 }
 
 function checkHours(existed, toAdd) {
     const splitedExisted = existed.split(':');
     const splitedToAdd = toAdd.split(':');
     if(+splitedExisted[0] > +splitedToAdd[0]){
-        console.log('hour with hour');
       return false;
     }
-    else if(+splitedExisted[1] > +splitedToAdd[1]){
-        console.log('min with min');
+    else if(+splitedExisted[0] === +splitedToAdd[0] && +splitedExisted[1] > +splitedToAdd[1]){
         return false;
       }
     else if(+splitedExisted[0] === +splitedToAdd[0] && +splitedExisted[1] === +splitedToAdd[1]){
-        console.log('details');
-        console.log('existed');
-        console.log(+splitedExisted[0]);
-        console.log(+splitedExisted[1]);
-        console.log('shift');
-        console.log(+splitedToAdd[0]);
-        console.log(+splitedToAdd[1]);
-        console.log('same same'); 
         return false;
     }
     return true;
