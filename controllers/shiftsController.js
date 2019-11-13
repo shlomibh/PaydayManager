8d8e920ab6a16a7f2b3c2d53efe93917c833f5dc
oreny <orenys7@gmail.com>
2019-11-13 17:33:38 +0200

    Shifts per month

--- controllers/shiftsController.js
+++ controllers/shiftsController.js
@@ -6,44 +6,17 @@ async function postShift(req, res, next) {
     try {
         const shift = req.body.shift;
         const id = shift.employeeId;
-        let ans;
         const user = await User.findOne({
             _id: id
         });
         if (!user) return res.status(httpCodes.CONFLICT).send("there is no such user");
         const department = user.department;
         shift.department = department;
-        existedShift = await Shift.find({ employeeId: shift.employeeId, date: shift.date });
-        if(existedShift.length > 0){
-            if(shift.absent === "חופש" || shift.absent === "מחלה") { //if we want to add absent shift
-                existedShift.forEach(element => {
-                    if(element) return res.status(httpCodes.FORBIDDEN).send("there is shifts in this date");
-                });
-            }
-            else {
-                existedShift.forEach(element => {
-                    if(element.absent === "מחלה" || element.absent === "חופש")
-                        return res.status(httpCodes.FORBIDDEN).send("the user absent in this day");
-                    else{
-                        if(element._id !== shift._id){
-                            ans = checkHourValidation(res, element, shift);
-                            if(!ans){
-                                return res.status(httpCodes.CONFLICT).send("hours error");
-                            } 
-                        }
-                       }
-                });
-            }
-        }
-        console.log('existedShift');
-        console.log(existedShift);
-        if(ans || existedShift.length < 1){
-            const shiftFromDb = await Shift.create(shift);
-            console.log(shiftFromDb);
-            if (!shiftFromDb) return res.status(httpCodes.FORBIDDEN).send("cannot create this shift");
-            return res.status(httpCodes.OK).send(shiftFromDb);
-        } 
-        
+        console.log(shift);
+        const shiftFromDb = await Shift.create(shift);
+        if (!shiftFromDb) return res.status(httpCodes.FORBIDDEN).send("cannot create this shift");
+        console.log(shiftFromDb);
+        return res.status(httpCodes.OK).send(shiftFromDb);
     } catch (error) {
         next(error);
     }
@@ -56,7 +29,25 @@ async function getShifts(req, res, next) {
             employeeId: employeeId
         });
         if (!shifts) return res.status(httpCodes.FORBIDDEN).send("there is no shifts to that employee");
+        console.log(shifts);
         return res.status(httpCodes.OK).send(shifts);
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function getShiftsPerMonth(req, res, next) {
+    try {
+        const employeeId = req.params.id;
+        const month = req.params.month;
+        const year = req.params.year;
+        const shifts = await Shift.find({
+            employeeId: employeeId
+        });
+        if (!shifts) return res.status(httpCodes.FORBIDDEN).send("there is no shifts to that employee");
+        const filterredShifts = shifts.filter(s => s.date.split('/')[0] === month);
+        console.log(filterredShifts);
+        return res.status(httpCodes.OK).send(filterredShifts);
     } catch (error) {
         next(error);
     }
@@ -69,6 +60,7 @@ async function deleteShift(req, res, next) {
             _id: shiftId
         });
         if (!shift) return res.status(httpCodes.FORBIDDEN).send("There is no such shift");
+        console.log(shift);
         await Shift.deleteOne({
             _id: shift._id
         });
@@ -78,39 +70,10 @@ async function deleteShift(req, res, next) {
     }
 }
 
-function checkHourValidation(res, element, shift) {
-    const startFromDb = element.start;
-    const endFromDb = element.end;
-    if(!checkHours(startFromDb, shift.start)) {
-        return false;
-    }
-    else if(!checkHours(endFromDb, shift.end)){
-        return false;
-    }
-    else if(!checkHours(endFromDb, shift.start)){
-        return false;
-    }
-    return true;
-}
-
-function checkHours(existed, toAdd) {
-    const splitedExisted = existed.split(':');
-    const splitedToAdd = toAdd.split(':');
-    if(+splitedExisted[0] > +splitedToAdd[0]){
-      return false;
-    }
-    else if(+splitedExisted[0] === +splitedToAdd[0] && +splitedExisted[1] > +splitedToAdd[1]){
-        return false;
-      }
-    else if(+splitedExisted[0] === +splitedToAdd[0] && +splitedExisted[1] === +splitedToAdd[1]){
-        return false;
-    }
-    return true;
-  }
-
 const shiftsControllers = {
     postShift,
     getShifts,
+    getShiftsPerMonth,
     deleteShift
 };
-module.exports = shiftsControllers;
+module.exports = shiftsControllers
--- routes/shifts.js
+++ routes/shifts.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const shiftsController = require('../controllers/ShiftsController');
 router.route("/:id").post(shiftsController.postShift);
 router.route("/:id").get(shiftsController.getShifts);
+router.route("/:id/m=:month/y=:year").get(shiftsController.getShiftsPerMonth);
 router.route("/:id").delete(shiftsController.deleteShift);
 
 
