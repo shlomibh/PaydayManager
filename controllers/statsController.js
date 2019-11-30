4079723117d8440c7ac96303284b62f55092f148
oreny <orenys7@gmail.com>
2019-11-30 14:17:14 +0200

    refactor stats

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -5,69 +5,65 @@ const userController = require('./UsersController');
 
 async function lectorStats(req, res, next) {
     try {
-        const query = req.params.query; //תעודת זהות של המשתמש
         const month = req.params.month; // החודש אותו ביקש המשתמש לקבל
         const year = req.params.year; //השנה אותה ביקש המשתמש לבקש
         const finalArrayResult = [];
         const shifts = await Shift.find({ submitted: true });
         if (!shifts) return res.status(httpCodes.FORBIDDEN).send("there is no shifts"); // אם אין משמרות מחזיר הודעת שגיאה
         const filteredShifts = getFilteredShifts(shifts, month, year);
-        switch(query){
-            case 'canceled':
-                const canceledShifts = filteredShifts.filter(s => s.absent === 'ביטול');
-                if(!canceledShifts) 
-                    return res.status(httpCodes.OK).send(null);
-                const canceledRes = getStats('lector', canceledShifts);
-                if(!canceledRes) 
-                    return res.status(httpCodes.OK).send(null);
-                const cmaxUser = await userController.getUserById(req, res, next, canceledRes.maxID);
-                const cminUser = await userController.getUserById(req, res, next, canceledRes.minID);
-                const cFinalResult = {
-                    maxUser: cmaxUser, 
-                    maxCount: canceledRes.maxCount,
-                    minUser: cminUser,
-                    minCount: canceledRes.minCount
-                };
-                console.log(cFinalResult);
-                finalArrayResult.push(cFinalResult);
-                return res.status(httpCodes.OK).send(cFinalResult);
-            case 'sickness':
-                const sickShifts = filteredShifts.filter(s => s.absent === 'מחלה');
-                if(!sickShifts) 
-                    return res.status(httpCodes.OK).send(null);
-                const sickRes = getStats('lector', sickShifts);
-                if(!sickRes) 
-                    return res.status(httpCodes.OK).send(null);
-                const smaxUser = await userController.getUserById(req, res, next, sickRes.maxID);
-                const sminUser = await userController.getUserById(req, res, next, sickRes.minID);
-                const sFinalResult = {
-                    maxUser: smaxUser, 
-                    maxCount: sickRes.maxCount,
-                    minUser: sminUser,
-                    minCount: sickRes.minCount
-                };
-                console.log(sFinalResult);
-                finalArrayResult.push(sFinalResult);
-                return res.status(httpCodes.OK).send(sFinalResult);
-            case 'dayoff':
-                const offShifts = filteredShifts.filter(s => s.absent === 'חופש');
-                if(!offShifts) 
-                    return res.status(httpCodes.OK).send(null);
-                const offRes = getStats('lector', offShifts);
-                if(!offRes) 
-                    return res.status(httpCodes.OK).send(null);
-                    const omaxUser = await userController.getUserById(req, res, next, offRes.maxID);
-                    const ominUser = await userController.getUserById(req, res, next, offRes.minID);
-                    const oFinalResult = {
-                        maxUser: omaxUser, 
-                        maxCount: offRes.maxCount,
-                        minUser: ominUser,
-                        minCount: offRes.minCount
-                    };
-                    console.log(oFinalResult);
-                    finalArrayResult.push(oFinalResult);
-                    return res.status(httpCodes.OK).send(oFinalResult);
-            case 'extraHours':
+
+        const canceledShifts = filteredShifts.filter(s => s.absent === 'ביטול');
+        if(!canceledShifts) 
+            return res.status(httpCodes.OK).send(null);
+        const canceledRes = getStats('lector', canceledShifts);
+        if(!canceledRes) 
+            return res.status(httpCodes.OK).send(null);
+        const cmaxUser = await userController.getUserById(req, res, next, canceledRes.maxID);
+        const cminUser = await userController.getUserById(req, res, next, canceledRes.minID);
+        const cFinalResult = {
+            maxUser: cmaxUser, 
+            maxCount: canceledRes.maxCount,
+            minUser: cminUser,
+            minCount: canceledRes.minCount
+        };
+        finalArrayResult.push(cFinalResult);
+
+        const sickShifts = filteredShifts.filter(s => s.absent === 'מחלה');
+        if(!sickShifts) 
+            return res.status(httpCodes.OK).send(null);
+        const sickRes = getStats('lector', sickShifts);
+        if(!sickRes) 
+            return res.status(httpCodes.OK).send(null);
+        const smaxUser = await userController.getUserById(req, res, next, sickRes.maxID);
+        const sminUser = await userController.getUserById(req, res, next, sickRes.minID);
+        const sFinalResult = {
+            maxUser: smaxUser, 
+            maxCount: sickRes.maxCount,
+            minUser: sminUser,
+            minCount: sickRes.minCount
+        };
+        finalArrayResult.push(sFinalResult);
+    
+        const offShifts = filteredShifts.filter(s => s.absent === 'חופש');
+        if(!offShifts) 
+            return res.status(httpCodes.OK).send(null);
+        const offRes = getStats('lector', offShifts);
+        if(!offRes) 
+            return res.status(httpCodes.OK).send(null);
+        const omaxUser = await userController.getUserById(req, res, next, offRes.maxID);
+        const ominUser = await userController.getUserById(req, res, next, offRes.minID);
+        const oFinalResult = {
+            maxUser: omaxUser, 
+            maxCount: offRes.maxCount,
+            minUser: ominUser,
+            minCount: offRes.minCount
+        };
+        finalArrayResult.push(oFinalResult);
+
+    // FOR NOW!!! //
+        return res.status(httpCodes.OK).send(finalArrayResult);
+
+            // case 'extraHours':
                 const extraShifts = filteredShifts.filter(s => s.absent === 'שעות נוספות'); //need to create new
                 if(!extraShifts) 
                     return res.status(httpCodes.OK).send(null);
@@ -84,8 +80,8 @@ async function lectorStats(req, res, next) {
                     };
                     console.log(eFinalResult);
                     finalArrayResult.push(eFinalResult);
-                    return res.status(httpCodes.OK).send(eFinalResult);
-                case 'inTime':
+                //     return res.status(httpCodes.OK).send(eFinalResult);
+                // case 'inTime':
                 const inTimeShifts = filteredShifts.filter();//s => s.lectorSubmittedDate <= '26/${month}/${year}'); 
                 if(!inTimeShifts) 
                     return res.status(httpCodes.OK).send(null);
@@ -104,9 +100,9 @@ async function lectorStats(req, res, next) {
                 finalArrayResult.push(itFinalResult);
                 return res.status(httpCodes.OK).send(itFinalResult);
             
-                default:
-                return res.status(httpCodes.BAD_REQUEST).send("wrong input");
-        }
+                // default:
+                // return res.status(httpCodes.BAD_REQUEST).send("wrong input");
+        // }
     } catch (error) {
         next(error);
     }
@@ -114,65 +110,64 @@ async function lectorStats(req, res, next) {
 
 async function departmentStats(req, res, next) {
     try {
-        const query = req.params.query; //תעודת זהות של המשתמש
         const month = req.params.month; // החודש אותו ביקש המשתמש לקבל
         const year = req.params.year; //השנה אותה ביקש המשתמש לבקש
-       
+        const finalArrayResult = [];
+
         const shifts = await Shift.find({ submitted: true });
         if (!shifts) return res.status(httpCodes.FORBIDDEN).send("there is no shifts"); // אם אין משמרות מחזיר הודעת שגיאה
         const filteredShifts = getFilteredShifts(shifts, month, year);
         
-        switch(query){
-            case 'canceled':
-                    const canceledShifts = filteredShifts.filter(s => s.absent === 'ביטול');
-                    if(!canceledShifts) 
-                        return res.status(httpCodes.OK).send(null);
-                    const canceledRes = getStats('department', canceledShifts);
-                    if(!canceledRes) 
-                        return res.status(httpCodes.OK).send(null);
-                    return res.status(httpCodes.OK).send(canceledRes);
-                case 'sickness':
-                    const sickShifts = filteredShifts.filter(s => s.absent === 'מחלה');
-                    if(!sickShifts) 
-                        return res.status(httpCodes.OK).send(null);
-                    const sickRes = getStats('department', sickShifts);
-                    if(!sickRes) 
-                        return res.status(httpCodes.OK).send(null);
-                    return res.status(httpCodes.OK).send(sickRes);
-                case 'dayoff':
-                    const offShifts = filteredShifts.filter(s => s.absent === 'חופש');
-                    if(!offShifts) 
-                        return res.status(httpCodes.OK).send(null);
-                    const offRes = getStats('department', offShifts);
-                    if(!offRes) 
-                        return res.status(httpCodes.OK).send(null);
-                    return res.status(httpCodes.OK).send(offRes);
-                case 'extraHours':
-                    const extraShifts = filteredShifts.filter(s => s.absent === 'שעות נוספות'); //need to create new
-                    if(!extraShifts) 
-                        return res.status(httpCodes.OK).send(null);
-                    const extraRes = getStats('department', extraShifts);
-                    if(!extraRes) 
-                        return res.status(httpCodes.OK).send(null);
-                    return res.status(httpCodes.OK).send(extraRes);
-                case 'inTime':
-                    const inTimeShifts = filteredShifts.filter();//s => s.lectorSubmittedDate <= '26/${month}/${year}'); 
-                    if(!inTimeShifts) 
-                        return res.status(httpCodes.OK).send(null);
-                    const inTimeRes = getStats('department', inTimeShifts);
-                    if(!inTimeRes) 
-                        return res.status(httpCodes.OK).send(null);
-                    return res.status(httpCodes.OK).send(inTimeRes);
-            default:
-                return res.status(httpCodes.BAD_REQUEST).send("wrong input");
-        }
+        const canceledShifts = filteredShifts.filter(s => s.absent === 'ביטול');
+        if(!canceledShifts) 
+            return res.status(httpCodes.OK).send(null);
+        const canceledRes = getStats('department', canceledShifts);
+        if(!canceledRes) 
+            return res.status(httpCodes.OK).send(null);
+        finalArrayResult.push(canceledRes);
+
+        const sickShifts = filteredShifts.filter(s => s.absent === 'מחלה');
+        if(!sickShifts) 
+            return res.status(httpCodes.OK).send(null);
+        const sickRes = getStats('department', sickShifts);
+        if(!sickRes) 
+            return res.status(httpCodes.OK).send(null);
+        finalArrayResult.push(sickRes);
+
+        const offShifts = filteredShifts.filter(s => s.absent === 'חופש');
+        if(!offShifts) 
+            return res.status(httpCodes.OK).send(null);
+        const offRes = getStats('department', offShifts);
+        if(!offRes) 
+            return res.status(httpCodes.OK).send(null);
+        finalArrayResult.push(offRes);
+        // FOR NOW!!! //
+        return res.status(httpCodes.OK).send(finalArrayResult);
+
+            
+        // case 'extraHours':
+            const extraShifts = filteredShifts.filter(s => s.absent === 'שעות נוספות'); //need to create new
+            if(!extraShifts) 
+                return res.status(httpCodes.OK).send(null);
+            const extraRes = getStats('department', extraShifts);
+            if(!extraRes) 
+                return res.status(httpCodes.OK).send(null);
+        //     return res.status(httpCodes.OK).send(extraRes);
+        // case 'inTime':
+            const inTimeShifts = filteredShifts.filter();//s => s.lectorSubmittedDate <= '26/${month}/${year}'); 
+            if(!inTimeShifts) 
+                return res.status(httpCodes.OK).send(null);
+            const inTimeRes = getStats('department', inTimeShifts);
+            if(!inTimeRes) 
+                return res.status(httpCodes.OK).send(null);
+
     } catch (error) {
         next(error);
     }
 }
 
 function getFilteredShifts(shifts, month, year) {
-    month='11';
+    month='11'; //need to remove!!!!
     const filterredShifts = shifts.filter(s => s.date.split('/')[0] === month); /// ״מסנן משמרות לפי חודש :הפונקציה לוקחת את התאריך שהתקבל ומפרקת אותה למערך לפי התו ומחזירה את הערך שבמקום הראשון    Shlomi: [0]
     if(!filterredShifts) 
         return res.status(httpCodes.FORBIDDEN).send("no shifts");
@@ -243,9 +238,7 @@ function getStats(identify, shifts) {
         maxID = minID;
         max = min;
     }
-    console.log(statList);
     const res = {maxID: maxID, maxCount: max, minID: minID, minCount: min};
-    console.log(res);
     return res;
 }
 
--- routes/statistics.js
+++ routes/statistics.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const statsController = require('../controllers/statsController');
-router.route("/lectors/q=:query/m=:month/y=:year").get(statsController.lectorStats);
-router.route("/department/q=:query/m=:month/y=:year").get(statsController.departmentStats);
+router.route("/lectors/m=:month/y=:year").get(statsController.lectorStats);
+router.route("/department/m=:month/y=:year").get(statsController.departmentStats);
 
 module.exports = router;
